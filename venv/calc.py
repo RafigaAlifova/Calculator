3f5c4b3763728725ec6e146e2782ab7df697892b
rafigaalifova <aliffovar@mail.ru>
2022-04-09 14:01:15 +0400

    creating the frames

--- venv/calc.py
+++ venv/calc.py
@@ -1,13 +1,26 @@
 import tkinter as tk
 
-
+LIGHT_GRAY='#F5F5F5'
 class Calculator:
     def __init__(self):
         self.window = tk.Tk()
         self.window.geometry("375x667")
         self.window.title("Calculator")
         self.window.resizable(0, 0)
-
+        
+        self.display.frame = self.create_display_frame()
+        self.button.frame = self.create_button_frame()
+        
+    def create_display_frame(self):
+        frame = tk.Frame(self.window,height=221,bg=LIGHT_GRAY)
+        frame.pack( fill="both", expand=True)
+        return frame
+    
+    def create_button_frame(self):
+        frame=tk.Frame(self.window)
+        frame.pack(fill="both", expand=True)
+        return frame
+        
     def run(self):
         self.window.mainloop()
 
@@ -15,3 +28,4 @@ class Calculator:
 if __name__ == "__main__":
     calc = Calculator()
     calc.run()
+    
